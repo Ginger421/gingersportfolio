292f8fded0abf04eb391b0d982f27ad2f5023168
Ginger Owen <gingerowen0421@gmail.com>
2023-03-28 13:15:08 -0600

    added some style to resume page

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -7,12 +7,12 @@ const Resume = () => {
 
   return (
     <div>
-        <p>
+        <p className='resume'>
             If you would like to know more about my training or work experience please click the button to download my resume.
         </p>
         
         <a href={VirginiaOwenResume} download='VirginiaOwenResume' target='blank'>
-            <button type='button'>Download</button> 
+            <button type='button' className='resume'>Download</button> 
         </a>
     </div>
   )
