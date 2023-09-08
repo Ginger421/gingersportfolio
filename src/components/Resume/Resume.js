2986c0d8f1bbeb47b7fd0256e49a2ab065018b58
Ginger Owen <gingerowen0421@gmail.com>
2023-09-08 15:13:25 -0600

    updated aboutme css

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -6,13 +6,13 @@ import VirginiaOwenResume from './VirginiaOwenResume.pdf'
 const Resume = () => {
 
   return (
-    <div>
-        <p className='resume'>
+    <div className='resume'>
+        <p >
             If you would like to know more about my training or work experience please click the button to download my resume.
         </p>
         
         <a href={VirginiaOwenResume} download='VirginiaOwenResume' target='blank'>
-            <button type='button' className='resume'>Download</button> 
+            <button type='button'>Download</button> 
         </a>
     </div>
   )
