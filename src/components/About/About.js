724d44148413e47555e74b63e03c73ac60fe7014
Ginger Owen <gingerowen0421@gmail.com>
2023-09-10 17:58:49 -0600

    updated css on about me page

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -19,11 +19,11 @@ function About() {
         <Grid2  container padding={1} spacing={1} flexDirection="row">
             
             <Grid2 xs={12} md={8} className="aboutMain">
-                <Grid2 xs={10} md={4} >
+                <Grid2 xs={10} md={4} className="pictureContainer">
                     <Avatar className="myPic" alt="Virginia Owen" src={pic} sx={{ width: 1, height: 1 }}  />
                   
                 </Grid2>
-                <Grid2 xs={12} md={6}>
+                <Grid2 xs={12} md={6} className="aboutContainer">
                       <p className="about-p" xs={12} md={4} lg={3}>
                         Hi and thank you for taking the time to check out my page! I am a web developer based in Denver, Colorado. I love the magic of writing code and seeing it rendered on a web page! I recently earned a certificate for completing the University of Denver's Full Stack Coding Boot Camp. I have experience working with HTML5, CSS3, JavaScript ES6, Node.js, Express.js, SQL, MongoDB, React.js, Tailwind CSS and Bootstrap. 
                     </p> 
